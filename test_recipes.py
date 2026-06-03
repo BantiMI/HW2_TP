908a3bfaf449c716d653d1338b25886416c9c699
Иванов Максим <ivanovmaksim10012001@gmail.com>
2026-06-03 16:13:50 +0300

    Реализованы тесты для класса Ingredient Реализован кластер тестов, проверяющий корректность объявление атрибутов объектов, а также корректность работы методов __str__ и __eq__

--- test_recipes.py
+++ test_recipes.py
@@ -1,5 +1,32 @@
 import pytest
 from HW2 import *
 
-def Test_Ingredient():
-    ...
+def test_Ingredient():
+    #Проверка инициализации атрибутов
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    assert Ingredient_1.name == "Ingredient_1"
+    assert Ingredient_1.quantity == 100
+    assert Ingredient_1.unit == "kg"
+
+    #Проверка метода __str__
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    assert Ingredient_1.__str__() == "Ingredient_1: 100.0 kg"
+
+    #Проверка метода __eq__
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_1", 200, "kg")
+    assert (Ingredient_1 == Ingredient_2) == True
+
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_2", 100, "kg")
+    assert (Ingredient_1 == Ingredient_2) == False
+
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "g")
+    Ingredient_2 = Ingredient("Ingredient_1", 100, "kg")
+    assert (Ingredient_1 == Ingredient_2) == False
+
+
+
+
+if __name__ == "__main__":
+    test_Ingredient()
