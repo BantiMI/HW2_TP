7f38a6512abb50e296d211dbaea8d9f6f83a25ee
Иванов Максим <ivanovmaksim10012001@gmail.com>
2026-06-03 23:17:54 +0300

    Тесты на класс ShoppingList Добавлен блок тестов на класс ShoppingList, проверяющий основные механики его работы, а именно: правильное добавление рецепта в список, выброс исключения при количестве порций <= 0, проверена механика удаление рецептов, объединения ингредиентов при выводе и сортировке, а также сложение листов.

--- test_recipes.py
+++ test_recipes.py
@@ -77,8 +77,89 @@ def test_Recipe():
     assert len(recipe) == 2
 
 
+def test_ShoppingList():
+    # Проверка выбрасывания исключения при порции <= 0
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_2", 200, "g")
+    recipe = Recipe("recipe1", [Ingredient_1, Ingredient_2])
+    lis = ShoppingList()
+    with pytest.raises(ValueError, match="Количество порций должно быть положительным"):
+        lis.add_recipe(recipe, 0)
+
+    # Проверка добавления рецента в список
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_2", 200, "g")
+    recipe = Recipe("recipe1", [Ingredient_1, Ingredient_2])
+    lis = ShoppingList()
+    lis.add_recipe(recipe, 1)
+    assert lis._items == [(Ingredient_1, "recipe1"), (Ingredient_2, "recipe1")]
+
+    # Проверка удаление всех ингредиентов по рецепту
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_2", 200, "g")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    recipe_2 = Recipe("recipe2", [Ingredient_2])
+    lis = ShoppingList()
+    lis.add_recipe(recipe_1, 1)
+    lis.add_recipe(recipe_2, 2)
+    lis.remove_recipe("recipe1")
+    assert lis._items == [(Ingredient_2, "recipe2")]
+
+    # Если такого рецепта нет - ничего не меняется
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    lis = ShoppingList()
+    lis.add_recipe(recipe_1, 1)
+    lis.remove_recipe("recipe2")
+    assert lis._items == [(Ingredient_1, "recipe1")]
+
+    # Одинаковые ингредиенты из разных рецептов суммируются
+    Ingredient_1 = Ingredient("Ingredient_1", 100, "kg")
+    Ingredient_2 = Ingredient("Ingredient_1", 200, "kg")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    recipe_2 = Recipe("recipe2", [Ingredient_2])
+    lis = ShoppingList()
+    lis.add_recipe(recipe_1, 1)
+    lis.add_recipe(recipe_2, 2)
+    assert lis.get_list() == [Ingredient("Ingredient_1", 500.0, "kg")]
+
+    # Возвращаемый список отсортирован по названию
+    Ingredient_1 = Ingredient("Ааа", 100, "kg")
+    Ingredient_2 = Ingredient("Ббб", 200, "kg")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    recipe_2 = Recipe("recipe2", [Ingredient_2])
+    lis = ShoppingList()
+    lis.add_recipe(recipe_1, 1)
+    lis.add_recipe(recipe_2, 2)
+    assert lis.get_list() == [Ingredient("Ааа", 100.0, "kg"), Ingredient("Ббб", 400.0, "kg")]
+
+    # Два списка корректно объединяются
+    Ingredient_1 = Ingredient("Ааа", 100, "kg")
+    Ingredient_2 = Ingredient("Ббб", 200, "kg")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    recipe_2 = Recipe("recipe2", [Ingredient_2])
+    lis_1 = ShoppingList()
+    lis_2 = ShoppingList()
+    lis_1.add_recipe(recipe_1, 1)
+    lis_2.add_recipe(recipe_2, 2)
+    lis = lis_1 + lis_2
+    assert lis.get_list() == [Ingredient("Ааа", 100.0, "kg"), Ingredient("Ббб", 400.0, "kg")]
+
+    # Исходные списки не изменяются
+    redient_2 = Ingredient("Ббб", 200, "kg")
+    recipe_1 = Recipe("recipe1", [Ingredient_1])
+    recipe_2 = Recipe("recipe2", [Ingredient_2])
+    lis_1 = ShoppingList()
+    lis_2 = ShoppingList()
+    lis_1.add_recipe(recipe_1, 1)
+    lis_2.add_recipe(recipe_2, 2)
+    lis = lis_1 + lis_2
+    assert lis_1.get_list() == [Ingredient("Ааа", 100.0, "kg")]
+    assert lis_2.get_list() == [Ingredient("Ббб", 400.0, "kg")]
+
 
 
 if __name__ == "__main__":
     test_Ingredient()
     test_Recipe()
+    test_ShoppingList()
